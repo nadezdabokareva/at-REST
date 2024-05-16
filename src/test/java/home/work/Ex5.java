62faa4551e0ef9d7d34df3bfbc32f3092965bf77
Nadezhda Bokareva <nadezhda.yureva@rt.ru>
2024-05-16 16:51:52 +0300

    II. Создание простых запросов к API ex6

---
+++ src/test/java/home/work/Ex5.java
@@ -0,0 +1,11 @@
+package home.work;
+
+import org.junit.jupiter.api.Test;
+
+public class Ex5 {
+
+    @Test
+    public void ex5(){
+
+    }
+}
---
+++ src/test/java/home/work/Ex6.java
@@ -0,0 +1,22 @@
+package home.work;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class Ex6 {
+    @Test
+    public void testRedirect() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false) //true=200
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
+    }
+}
