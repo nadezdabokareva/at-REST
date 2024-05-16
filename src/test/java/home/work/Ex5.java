9a0fc19b27368fe14b5e49b7a70399de49b78cfe
Nadezhda Bokareva <nadezhda.yureva@rt.ru>
2024-05-16 22:28:01 +0300

    II. Создание простых запросов к API ex8

--- src/test/java/home/work/Ex5.java
+++ src/test/java/home/work/Ex5.java
@@ -1,11 +1,27 @@
 package home.work;
 
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Ex5 {
 
     @Test
     public void ex5(){
+        Response response = RestAssured
+                .given()
+                .queryParam("message", "And this is a second message")
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .andReturn();
+
+        JsonPath extractor = response.jsonPath();
+        String message = String.valueOf(extractor.get("message").equals("And this is a second message"));
+        System.out.println(message);
+
 
     }
 }
---
+++ src/test/java/home/work/Ex8.java
@@ -0,0 +1,50 @@
+package home.work;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Ex8 {
+
+    @Test
+    public void testEx8()  {
+        //1 запрос без токена
+        JsonPath response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .jsonPath();
+
+        //достаем токен и время из первого запроса
+        String token = response.get("token");
+        System.out.println(token);
+        Integer seconds = response.getInt("seconds");
+        System.out.println(seconds);
+
+        //делаем второй запрос
+        JsonPath responseWithToken1 = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job/?token="+token)
+                .jsonPath();
+
+        responseWithToken1.prettyPrint();
+
+        //проверяем поле статус
+        Assertions.assertEquals(responseWithToken1.get("status"), "Job is NOT ready");
+//
+//        //ждем кол-во секунд из первого запроса
+//
+//
+//        //делаем запрос с токеном
+//        Response responseWithToken2 = RestAssured
+//                .given()
+//                .get("https://playground.learnqa.ru/ajax/api/longtime_job/?token="+token)
+//                .andReturn();
+//        responseWithToken2.prettyPrint();
+//
+//
+//        //проверяем поле статус
+//        Assertions.assertEquals(response.get("status"), "Job is ready");
+    }
+}
