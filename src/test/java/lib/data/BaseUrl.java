4aa4b27eceff66a8fa69acc99b4a2a85c9ee1311
Nadezhda Bokareva <nadezhda.yureva@rt.ru>
2024-05-28 13:12:09 +0300

    Задание 19 запуск тестов на dev-окуржении

--- src/test/java/lib/data/BaseUrl.java
+++ src/test/java/lib/data/BaseUrl.java
@@ -1,13 +1,17 @@
 package lib.data;
 
 public class BaseUrl {
+
+    //https://playground.learnqa.ru/api_dev/
+    //https://playground.learnqa.ru
+
     public static String baseUrl = "https://playground.learnqa.ru";
-    public static String userLogin = "/api/user/login";
-    public static String userAuth = "/api/user/auth";
-    public static String userRegistration = "/api/user/";
+    public static String userLogin = "/api_dev/user/login";
+    public static String userAuth = "/api_dev/user/auth";
+    public static String userRegistration = "/api_dev/user/";
 
     public static String userCard (String id) {
-        String userCard = "/api/user/"+id;
+        String userCard = "/api_dev/user/"+id;
         return userCard;
     }
 
